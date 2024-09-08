3ea071b6dc2cff6f5bc701e5e5442c77ff741899
Velozitty <kelvinveloz19@gmail.com>
2024-09-07 21:23:07 -0400

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,56 @@
+#Day 2: 30 Days of python programming
+
+first_name = "Kelvin"
+last_name = "Veloz"
+full_name = first_name + " " + last_name
+country  = "USA"
+city = "Port Chester"
+age = 23
+year = 2024
+is_married = False 
+is_true = False
+is_light_on = True
+
+dog_name, dog_age, dog_breed = "Boost", 9, "Pomsky"
+
+print(full_name)
+
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_light_on))
+print(type(dog_age))
+
+
+print(len(first_name))
+print(len(first_name) > len(last_name))
+
+num_one = 5
+num_two = 4
+total = num_one + num_two
+print(total)
+diff = num_one - num_two
+print(diff)
+product = num_one * num_two
+print(product)
+division = num_one / num_two
+print(division)
+remainder = num_two % num_one
+print(remainder)
+exp =  num_one ** num_two
+print(exp)
+floor_division = num_two // num_two
+print(floor_division)
+radius = 30
+pi = 3.14
+area_of_circle = pi * float((radius *radius))
+print(area_of_circle)
+circum_of_circle = 2 * pi * radius
+print(circum_of_circle)
+rad = float(input("Enter a Radius: "))
+area_of_circle = pi * float(rad*rad)
+print(area_of_circle)
